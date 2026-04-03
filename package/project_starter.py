0f54f9be6836bcc26967248a2ec21bcfc77b4960
Цветков Дмитрий Сергеевич <TsvetkovDS@TRCONT.RU>
2026-04-03 08:41:48 +0300

    fix bug folders creator

--- package/project_starter.py
+++ package/project_starter.py
@@ -7,12 +7,9 @@ sys.path.append(os.getcwd())
 from package.config import folders_rules_dict
 
 def folders_maker(): 
-    folders_created = False
-
 
     print(Fore.BLUE + f'Проверяем наличие нужных папок в проекте...' + Fore.RESET, end='')  
-    sources_folder = os.path.join(os.getcwd(), 'Исходники') 
-    folders = list(os.walk(sources_folder))[0][1]
+    folders_created = False
 
     for folder in ['Исходники', 'Подготовленные', 'Загруженные']:
         if not os.path.exists(folder):
@@ -20,7 +17,9 @@ def folders_maker():
             folders_created = True
             print(Fore.GREEN + '\nСоздали папку ' + Fore.WHITE + fr'"\{folder}"' + Fore.RESET, end='')            
 
-    
+    sources_folder = os.path.join(os.getcwd(), 'Исходники') 
+    folders = list(os.walk(sources_folder))[0][1]
+
     for k in folders_rules_dict.keys():
         if k not in folders:
             os.mkdir(os.path.join(sources_folder, k))
@@ -31,6 +30,6 @@ def folders_maker():
         print(Fore.GREEN + f'Ok' + Fore.RESET, end='')  
 
     print() 
-    
+
 if __name__ == '__main__':
     folders_maker()
