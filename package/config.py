77931d7d9ca8cc00955b6bb112f021bedf0af770
Tsvetkov Dmitry <DSTsvetkovPRO@yandex.ru>
2026-04-06 18:11:08 +0300

    add vsk otkrep

--- package/config.py
+++ package/config.py
@@ -1,19 +1,68 @@
 folders_rules_dict = {
-    "Согаз_изменение объёма": {
+    "Альфа_открепление": {
         "processor_name": "base",
-        "skiprows": 21,
-        "filter_not_empty": "Фамилия",
+        "skiprows": 0,
+        "filter_not_empty": "row_number",
         "source_header": [
-            "№ п/п", "Фамилия", "Имя", "Отчество", "Дата рождения", "№ полиса", "Пол", "Прежний объем обслуживания", "Новый объем обслуживания",
-            "Дата начала обслуживания","Дата окончания обслуживания","Место работы (Страхователь)"
+            "row_number", "policy_number", "date_from", "date_to", "date_cancel", "prog_code", "fio", "prog_name",
+            "add_phone", "address", "phone_home", "phone_office", "birth_date", "person_sex", "insurer", "ag_number",
+            "our_subj", "hosp_extrim", "hospital", "ambulance", "per_title", "group_name", "company_of_work",
+            "place_of_work", "history_num", "citizen", "new_policy", "policy_omc", "docum",
+            "degree_of_relationship","per_mobile_phone", "servicing_office", "adr_country", "adr_post_code",
+            "adr_city", "adr_region", "adr_street", "adr_house", "adr_building", "adr_appartment", "adr_entrance",
+            "adr_access_code", "adr_floor", "undgr_station", "per_last_name", "per_first_name", "per_middle_name", 
+            "pdoc_ser",	"pdoc_no", "pdoc_issue_date", "pdoc_issue_place", "lpu_number","risk_code",	"age_id",
+            "pay_type", "med_prog_short"
         ],
         "result_columns": {
-            "aaa": {
-                "source": "column", "value": "aaaaaaaa"
+            "Номер полиса": {
+                "source": "column", "value": "policy_number"
             },
-            "adfsdfsdafasdaa": {
-                "source": "default_value", "value": "45.7"
-            }            
+            "Дата открепления": {
+                "source": "column", "value": "date_cancel"
+            },
+            "Дата рождения": {
+                "source": "column", "value": "birth_date"
+            },
+            "Фамилия": {
+                "source": "column", "value": "per_last_name"
+            }, 
+            "Имя": {
+                "source": "column", "value": "per_first_name"
+            },    
+            "Отчество": {
+                "source": "column", "value": "per_middle_name"
+            }                                                         
+        }
+    },
+        "ВСК_открепление": {
+        "processor_name": "base",
+        "skiprows": 5,
+        "filter_not_empty": "NAME1",
+        "source_header": [
+            "npp", "NAME1", "NAME2", "NAME3", "NIB", "DATE", "SEX", "POLIC", "POLIC SER", "ADDRESS P",
+            "TEL1", "KATEGORY", "PLACE", "Holding", "BEGIN", "END"
+        ],
+        "result_columns": {
+            "Номер полиса": {
+                "source": "column", "value": "POLIC SER"
+            },
+            "Дата открепления": {
+                "source": "column", "value": "END"
+            },
+            "Дата рождения": {
+                "source": "column", "value": "DATE"
+            },
+            "Фамилия": {
+                "source": "column", "value": "NAME1"
+            }, 
+            "Имя": {
+                "source": "column", "value": "NAME2"
+            },    
+            "Отчество": {
+                "source": "column", "value": "NAME3"
+            }                                                         
         }
     }
 }
+
